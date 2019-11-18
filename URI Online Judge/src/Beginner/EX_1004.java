ddd6f0177afc47281bc8d06e3beddb398da609ef
Samuel Modesto <samuelmodestoes@gmail.com>
2019-11-18 17:05:39 -0300

    ADDED: Exercise_1004 solved

---
+++ URI Online Judge/src/Beginner/EX_1004.java
@@ -0,0 +1,24 @@
+package Beginner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class EX_1004 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		InputStreamReader inputReader = new InputStreamReader(System.in);
+		BufferedReader input = new BufferedReader(inputReader);
+		
+		int a, b, prod;
+		
+		a = Integer.parseInt(input.readLine());
+		b = Integer.parseInt(input.readLine());
+		
+		prod = a*b;
+		
+		System.out.println("PROD = " + prod);
+		
+
+	}
+
+}
