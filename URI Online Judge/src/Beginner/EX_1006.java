41c3b3b9dbb0785a1b6c046f683cfea6128c712c
Samuel Modesto <samuelmodestoes@gmail.com>
2019-11-18 17:07:56 -0300

    ADDED: Exercise_1006 solved

---
+++ URI Online Judge/src/Beginner/EX_1006.java
@@ -0,0 +1,39 @@
+package Beginner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class EX_1006 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		InputStreamReader inputReader = new InputStreamReader(System.in);
+		BufferedReader input = new BufferedReader(inputReader);
+
+		double a, b, c, media;
+		
+		a = Double.parseDouble(input.readLine());
+		b = Double.parseDouble(input.readLine());
+		c = Double.parseDouble(input.readLine());
+		
+		
+//		Scanner entrada = new Scanner(System.in);
+//		System.out.println("type the values: ");		
+//		a = entrada.nextDouble();
+//		b = entrada.nextDouble();
+//		c = entrada.nextDouble();
+		
+		media = (2*a + 3*b + 5*c)/10;
+
+		
+		NumberFormat formatter = new DecimalFormat("0.0");
+		
+		System.out.println("MEDIA = " + formatter.format(media));
+		
+		
+
+	}
+
+}
