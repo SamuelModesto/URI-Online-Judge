667f24bfaa222127e67ff031c587774d5d598949
Samuel Modesto <samuelmodestoes@gmail.com>
2019-11-19 17:06:09 -0300

    ADDED: Exercise_1012 solving

--- URI Online Judge/src/Beginner/Ex_1012.java
+++ URI Online Judge/src/Beginner/Ex_1012.java
@@ -4,20 +4,22 @@ import java.util.Scanner;
 
 public class Ex_1012 {
 
- public static void main(String[] args) {
-  double a, b, c;
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
-  Scanner input = new Scanner(System.in);
-  a = input.nextDouble();
-  b = input.nextDouble();
-  c = input.nextDouble();
+		double a, b, c;
+		double pi = 3.14159;
+		// Leitura dos dados
+		a = input.nextDouble();
+		b = input.nextDouble();
+		c = input.nextDouble();	
+		
+		System.out.printf("TRIANGULO: %.3f\n", a * c / 2);
+		System.out.printf("CIRCULO: %.3f\n",  (c * c) * pi );
+		System.out.printf("TRAPEZIO: %.3f\n", ((a + b) / 2) * c);
+		System.out.printf("QUADRADO: %.3f\n", b * b);
+		System.out.printf("RETANGULO: %.3f\n", a * b);
 
-  System.out.printf("TRIANGULO: %.3f\n", (a * c) / 2);
-  System.out.printf("CIRCULO: %.3f\n", c * c * 3.14159);
-  System.out.printf("TRAPEZIO: %.3f\n", ((a + b) / 2) * c);
-  System.out.printf("QUADRADO: %.3f\n", b * b);
-  System.out.printf("RETANGULO: %.3f\n", a * b);
-
- }
+	}
 
 }
