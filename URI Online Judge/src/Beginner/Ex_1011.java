f0d69d6b6a0ec37ddae31e96cd0d0848748ec1c0
Samuel Modesto <samuelmodestoes@gmail.com>
2019-11-19 12:40:16 -0300

    ADDED: Exercise_1011 solved

---
+++ URI Online Judge/src/Beginner/Ex_1011.java
@@ -0,0 +1,24 @@
+package Beginner;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ex_1011 {
+
+	public static void main(String[] args)throws IOException {  
+		Scanner input = new Scanner(System.in);
+		
+		double pi = 3.14159;
+		double R;
+		double total;
+		
+		R = input.nextDouble();
+		
+		total = 4/3.0*pi * (R*R*R);
+		
+		System.out.printf("VOLUME = %.3f\n", total);
+		
+
+	}
+
+}
