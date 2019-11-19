cc1e3a9d0a1adfea78027f97a023081d856ee92c
Samuel Modesto <samuelmodestoes@gmail.com>
2019-11-19 16:56:39 -0300

    ADDED: Exercise_1012 solving

--- URI Online Judge/src/Beginner/Ex_1012.java
+++ URI Online Judge/src/Beginner/Ex_1012.java
@@ -7,33 +7,18 @@ public class Ex_1012 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		double a, b, c, areaTriagulo, areaCirculo, areaTrapezio, areaQuadrado, areaRetangulo;
-		double pi = 3.14159;
+		double a, b, c;
+		
 		// Leitura dos dados
 		a = input.nextDouble();
 		b = input.nextDouble();
-		c = input.nextDouble();
-
-		// Área do triangulo retangulo
-		areaTriagulo = a * c / 2;
-
-		// Área do círculo
-		areaCirculo = pi * (c * c);
-
-		// Área do trapézio
-		areaTrapezio = (a + b) * c / 2;
-
-		// Área do quadrado
-		areaQuadrado = b * b;
+		c = input.nextDouble();	
 		
-		//Área do retângulo
-		areaRetangulo = a * b;
-		
-		System.out.printf("TRIANGULO: %3.f\n", areaTriagulo);
-		System.out.printf("CIRCULO: %3.f\n", areaCirculo);
-		System.out.printf("TRAPEZIO: %3.f\n", areaTrapezio);
-		System.out.printf("QUADRADO: %3.f\n", areaQuadrado);
-		System.out.printf("RETANGULO: %3.f\n", areaRetangulo);
+		System.out.printf("TRIANGULO: %3.f\n", a * c / 2);
+		System.out.printf("CIRCULO: %3.f\n",  (c * c) * 3.14159);
+		System.out.printf("TRAPEZIO: %3.f\n", (a + b) * c / 2);
+		System.out.printf("QUADRADO: %3.f\n", b * b);
+		System.out.printf("RETANGULO: %3.f\n", a * b);
 
 	}
 
