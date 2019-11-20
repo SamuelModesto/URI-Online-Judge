043cd72bf79b559b28de7145b896dfe3842c6b33
Samuel Modesto <samuelmodestoes@gmail.com>
2019-11-20 16:14:15 -0300

    ADDED: Exercise_1014 solved

---
+++ URI Online Judge/src/Beginner/Ex_1014.java
@@ -0,0 +1,27 @@
+package Beginner;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Ex_1014 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int distancia;
+		double gastoCombustivel;
+		double autonomia;
+		
+		distancia = input.nextInt();
+		gastoCombustivel = input.nextDouble();
+		
+		NumberFormat formatter = new DecimalFormat("#.000");
+		
+		autonomia = distancia / gastoCombustivel;
+		
+		System.out.println(formatter.format(autonomia) + " km/l");
+
+	}
+
+}
