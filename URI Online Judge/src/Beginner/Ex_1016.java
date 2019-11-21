ceeb2387a11acef13f29a7a1813ef35006adcb23
Samuel Modesto <samuelmodestoes@gmail.com>
2019-11-21 17:31:58 -0300

    ADDED: Exercise_1016 solved

--- URI Online Judge/src/Beginner/Ex_1016.java
+++ URI Online Judge/src/Beginner/Ex_1016.java
@@ -1,9 +1,20 @@
 package Beginner;
 
+import java.util.Scanner;
+
 public class Ex_1016 {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		
+		int distancia;
+		int tempo;
+		
+		distancia = input.nextInt();
+		
+		tempo = distancia * 2 ;
+		
+		System.out.println(tempo + " minutos");
 	}
 
 }
