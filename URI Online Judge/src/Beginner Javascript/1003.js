ecbdab6d3981a645496073f1f342427535a4d969
MODESTO <samuelmodestoes@gmail.com>
2020-01-05 13:04:31 -0200

    ADDED: Exercise_1003 solved

--- URI Online Judge/src/Beginner Javascript/1003.js
+++ URI Online Judge/src/Beginner Javascript/1003.js
@@ -1,8 +1,7 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
-
 var num1 = parseInt(lines.shift());
 var num2 = parseInt(lines.shift());
 
-console.log(`X = ${num1 + num2}`);
+console.log("SOMA = "+ (num1 + num2));
