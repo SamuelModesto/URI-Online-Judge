cca80cc70b725cc25cc6967a24c9a99b3710ee0c
MODESTO <samuelmodestoes@gmail.com>
2020-01-05 20:53:44 -0200

    ADDED: Exercise_1006 solved

---
+++ URI Online Judge/src/Beginner Javascript/1006.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+
+var A = parseFloat(lines.shift());
+var B = parseFloat(lines.shift());
+var C = parseFloat(lines.shift());
+var MEDIA = (A * 2 + B * 3 + C * 5) / 10;
+
+console.log("MEDIA = " + MEDIA.toFixed(1));
